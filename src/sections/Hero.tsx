6a550d486a66c7b82d32e8529d1c382ed5937178
sasha1107 <sasha1107@naver.com>
2024-08-18 01:48:47 +0900

    feat: Hero 추가

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <section className="h-main p-20 flex justify-center items-center bg-fixed">
+      <div className="w-[600px] aspect-square bg-[url('/bg.png')] bg-contain back" />
+    </section>
+  );
+};
+
+export default Hero;
