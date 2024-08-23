00eecb1633ace44d75154946151dcf0c2e5e5fce
sasha1107 <sasha1107@naver.com>
2024-08-23 15:03:33 +0900

    feat: image path 설정

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const prefix =
+  process.env.NODE_ENV === "production"
+    ? "https://sasha1107.github.io/framer-sample"
+    : "";
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,7 +1,18 @@
+import { cn } from "@/lib/utils";
+import { twJoin } from "tailwind-merge";
+
 const Hero = () => {
   return (
     <section className="h-main p-20 flex justify-center items-center bg-fixed">
-      <div className="w-[600px] aspect-square bg-[url('/bg.png')] bg-contain back" />
+      <div
+        className={cn(
+          "w-[600px] aspect-square bg-[url('/bg.png')] bg-contain back",
+          twJoin(
+            process.env.NODE_ENV === "production" &&
+              `bg-[url('https://sasha1107.github.io/framer-sample/bg.png')]`
+          )
+        )}
+      />
     </section>
   );
 };
